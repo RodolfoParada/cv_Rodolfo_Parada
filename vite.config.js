7c74ca1aa2cdd7383cbff56b4f0e52507817a761
Rodolfo <rodolfo.parada.gonzalez@gmail.com>
2024-11-05 00:24:40 -0300

    f

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
+import {
+  defineConfig
+} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
- base: '/cv_rodolfo_parada/',
+  base: process.env.NODE_ENV === 'production' ? '/cv_rodolfo_parada/' : '/',
   plugins: [vue()],
 })
