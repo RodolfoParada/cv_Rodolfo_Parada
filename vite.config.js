3196fabf4b735a496a75694b391adc089f4bb5a1
Rodolfo <73254069+RodolfoParada@users.noreply.github.com>
2024-11-04 23:24:03 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
  base: '/cv_rodolfo_parada/',
-=======
-  base: '/cv_rodolfo_parada',
->>>>>>> refs/remotes/origin/main
   plugins: [vue()],
 })
